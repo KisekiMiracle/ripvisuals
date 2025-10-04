b34a0977d047cb3d66ef8e326f699f7cba264408
Kiseki 奇跡 <contact@kiseki-miracle.dev>
2025-10-03 23:58:38 -0500

    updated tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +12,5 @@ export default defineConfig({
   },
 
   integrations: [icon()],
+  adapter: cloudflare(),
 });
